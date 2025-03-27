d96b44ef793bd129046ae0e19331fc7aec2ae8ab
Joseph JK VIT <115476354+JJKVIT@users.noreply.github.com>
2025-03-27 09:33:33 +0530

    Player lock on intial animation

--- Assets/Initial_Zoom_out.cs
+++ Assets/Initial_Zoom_out.cs
@@ -10,8 +10,16 @@ public class Initial_Zoom_Out : MonoBehaviour
     [SerializeField] float camSpeed = 0.1f;
     [SerializeField] float acceleration = 1.05f; 
     [SerializeField] float maxDistance;
+    [SerializeField] public GameObject Player;
+
+    void Start(){
+        Player.GetComponent<Rigidbody2D>().gravityScale = 40f;
+    }
 
     void Update(){
+        if(camDistance == maxDistance){
+            Player.GetComponent<Rigidbody2D>().gravityScale = 3f;
+        }
         if (vcam != null && camDistance < maxDistance){
             camDistance = Mathf.MoveTowards(camDistance, maxDistance, camSpeed * Time.deltaTime);
             vcam.m_Lens.OrthographicSize = camDistance;
