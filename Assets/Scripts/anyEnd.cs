c1811df913fcd5c5f5297d3f477d00c5f717d144
Joseph JK VIT <115476354+JJKVIT@users.noreply.github.com>
2025-04-25 18:16:11 +0530

    Level1 autoTransition, Dock Ladder placed

--- Assets/Scripts/anyEnd.cs
+++ Assets/Scripts/anyEnd.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class anyEnd : MonoBehaviour
 {
     [SerializeField] private float endDelay = 1f;
     [SerializeField] private float endSize;
-    
+    [SerializeField] private bool autoNext = false;
 
     private bool isEnd = false;
+
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.CompareTag("Player")){
+        if (autoNext){
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (other.CompareTag("Player")){
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
